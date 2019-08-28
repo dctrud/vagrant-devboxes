f5cae4d42413f20bd28d90148255267f9683c336
David Trudgian <david.trudgian@sylabs.io>
2019-08-28 09:37:55 -0500

    remove opensuse

--- common-rpm-suse/provision.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Now some base repo stuff
-zypper --non-interactive install automake autoconf libarchive-devel openssl-devel libuuid-devel git valgrind vim-enhance squashfsd
-
-zypper --non-interactive install go go-doc
-
